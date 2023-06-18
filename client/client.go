1125164beacc76431575a49d443749cc8b25b868
Arpit Srivastava <arpit.srivastava@gojek.com>
2023-06-18 22:57:36 +0530

    Testing go-grpc-pool library

--- client/client.go
+++ client/client.go
@@ -2,25 +2,57 @@ package main
 
 import (
 	"context"
+	"fmt"
+	grpcpool "github.com/processout/grpc-go-pool"
 	"google.golang.org/grpc"
 	"grpc-test/protos"
 	"log"
+	"time"
 )
 
 func main() {
-	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Client could not connect to server on 9000. [%s]", err)
+
+	factory := func() (*grpc.ClientConn, error) {
+		conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("Client could not connect to server on 9000. [%s]", err)
+		}
+		log.Printf("Connected to Server at : [%s]\n", ":9000")
+		return conn, err
 	}
 
-	defer conn.Close()
+	pool, err := grpcpool.New(factory, 5, 10, time.Second)
+	if err != nil {
+		log.Fatalf("Failed to create gRPC pool: %v", err)
+	}
 
+	for i := 1; i <= 10; i++ {
+		go func(i int) {
+			fmt.Printf("Calling Chat server for: [%d]\n", i)
+			callChatServer(pool)
+			fmt.Printf("Call completed for Chat server for: [%d]", i)
+		}(i)
+	}
+
+	time.Sleep(10 * time.Minute)
+}
+
+func callChatServer(pool *grpcpool.Pool) {
+	ctx := context.Background()
+	conn, err := pool.Get(ctx)
+	defer conn.Close()
+	if err != nil {
+		msg := "failed to connect to worker"
+		log.Printf("Error: [%s], msg: [%s]\n", err, msg)
+		return
+	}
 	c := protos.NewChatServiceClient(conn)
 	msg := &protos.Message{
 		Body: "Server! Are you there??",
 	}
 
 	resp, err := c.SayHello(context.Background(), msg)
+
 	if err != nil {
 		log.Fatalf("error received from server. Error is: [%s]\n", err)
 	}
--- client/go.mod
+++ client/go.mod
@@ -3,15 +3,16 @@ module grpc-test/client
 go 1.20
 
 require (
-	google.golang.org/grpc v1.55.0
+	google.golang.org/grpc v1.56.0
 	grpc-test/protos v0.0.0-00010101000000-000000000000
 )
 
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	github.com/processout/grpc-go-pool v1.2.1 // indirect
+	golang.org/x/net v0.11.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/text v0.10.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 )
